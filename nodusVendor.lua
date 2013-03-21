31dd2c1544f97ba7d0a95dd6f48f4c578320337b
NodusCursorius <nodus.cursorius@gmail.com>
2013-03-21 14:39:44 -0700

    Minor changes for "best practice" design
    Remove CreateFrame upvalue due to infrequent use. Change line 19
    variable i to descriptive variable bag and update variable usage. Change
    line 22 variable n to descriptive variable slot and update variable
    usage. Change line 31 variable k for conventional placeholder _ due to
    unused key data. Change line 43 if to elseif for code simplification.

--- nodusVendor.lua
+++ nodusVendor.lua
@@ -1,5 +1,5 @@
-local CreateFrame, GetMoney, GetContainerNumSlots, GetContainerNumFreeSlots, GetContainerItemID, GetItemInfo, GetCoinTextureString, ITEM_SOULBOUND =
-	CreateFrame, GetMoney, GetContainerNumSlots, GetContainerNumFreeSlots, GetContainerItemID, GetItemInfo, GetCoinTextureString, ITEM_SOULBOUND
+local GetMoney, GetContainerNumSlots, GetContainerNumFreeSlots, GetContainerItemID, GetItemInfo, GetCoinTextureString, ITEM_SOULBOUND, UseContainerItem =
+	GetMoney, GetContainerNumSlots, GetContainerNumFreeSlots, GetContainerItemID, GetItemInfo, GetCoinTextureString, ITEM_SOULBOUND, UseContainerItem
 
 local soulboundStrings = {}
 local soulboundTooltip = CreateFrame("GameTooltip")
@@ -16,21 +16,21 @@ local eventFrame = CreateFrame("FRAME")
 eventFrame:SetScript("OnEvent", function(self, event, ...)
 	if event == "MERCHANT_SHOW" then
 		profitStart = GetMoney()
-		for i = 1, 4 do
-			local bagSize = GetContainerNumSlots(i)
-			if bagSize ~= GetContainerNumFreeSlots(i) then
-				for n = 1, bagSize do
-					local itemID = GetContainerItemID(i, n)
+		for bag = 1, 4 do
+			local bagSize = GetContainerNumSlots(bag)
+			if bagSize ~= GetContainerNumFreeSlots(bag) then
+				for slot = 1, bagSize do
+					local itemID = GetContainerItemID(bag, slot)
 					if itemID then
 						local itemQuality, _, _, _, _, _, _, _, vendorValue =  select(3, GetItemInfo(itemID))
 						if vendorValue and vendorValue > 0 then
 							if itemQuality == 0 then
-								UseContainerItem(i,n)
+								UseContainerItem(bag,slot)
 							else
-								soulboundTooltip:SetBagItem(i, n)
-								for k, v in ipairs(soulboundStrings) do
+								soulboundTooltip:SetBagItem(bag, slot)
+								for _, v in ipairs(soulboundStrings) do
 									if v:GetText() == ITEM_SOULBOUND then
-										UseContainerItem(i,n)
+										UseContainerItem(bag,slot)
 										break
 									end
 								end
@@ -40,8 +40,7 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 				end
 			end
 		end
-	end
-	if event == "MERCHANT_CLOSED" then
+	elseif event == "MERCHANT_CLOSED" then
 		local profitEnd = GetMoney()
 		if profitStart < profitEnd then 
 			print("|cffffff00Profit:|r", GetCoinTextureString(profitEnd - profitStart))
